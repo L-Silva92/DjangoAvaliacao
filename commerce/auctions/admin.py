dfeed5400cff782cc2d189e8df295369967cfad8
vinibaldo <vinibaldo1@hotmail.com>
2021-05-12 12:20:56 -0300

    teste dos formularios

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from commerce.auctions.models import categ, leilao
 
 # Register your models here.
+admin.site.register(categ)
+admin.site.register(leilao)
--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -5,10 +5,17 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-class Flight(models.Model):
-    origin = models.ForeignKey(max_length=10)
-    destination = models.ForeignKey(max_length=10)
-    duration = models.IntegerField()
+class categ(models.Model):
+    cat = models.CharField(max_length=10)
+    def __str__(self):
+        return f"{self.id}: {self.cat}"
+
+class leilao(models.Model):
+    titulo = models.CharField(max_length=10)
+    descricao = models.CharField(max_length=10)
+    valor_min = models.IntegerField()
+    foto = models.ImageField()
+    categ = models.ForeignKey(categ,on_delete=models.CASCADE, related_name="cat")
  
     def __str__(self):
-        return f"{self.id}: {self.origin} to {self.destination}"
+        return f"{self.id}: {self.titulo} | {self.descricao} | {self.valor_min} | {self.foto} | {self.categ}"
