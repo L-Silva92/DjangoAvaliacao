e1d5fb23010f5642b64405a914249cdfc58843f3
vinibaldo <vinibaldo1@hotmail.com>
2021-05-12 11:46:21 -0300

    teste models
    criado o primeiro form de teste

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Flight(models.Model):
+    origin = models.ForeignKey(max_length=10)
+    destination = models.ForeignKey(max_length=10)
+    duration = models.IntegerField()
+ 
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"
